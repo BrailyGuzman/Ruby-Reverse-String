c0acbdce056c1ee1ff5ac168157e9920a6c6e97c
Braily G <94770717+BrailyGuzman@users.noreply.github.com>
2022-06-23 11:59:05 -0400

    Create reverse.rb

---
+++ reverse.rb
@@ -0,0 +1,12 @@
+puts "Enter The Word: "
+word = gets.chomp
+
+word_reverse = word.reverse
+puts "The Reversed Word is: "
+puts word_reverse
+
+if word_reverse == word
+    puts "This word is a palindrome"
+elsif
+    puts "This word is not a palindrome"
+end
